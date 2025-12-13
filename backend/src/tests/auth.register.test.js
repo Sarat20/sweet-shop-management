f59559a7bf7268c85ca0897ec50342ae16f624ff
Sarat20 <saratkumarkalagara2398s@gmail.com>
2025-12-13 17:02:47 +0530

    test: registered user must  be saved in db but not saved

--- backend/src/tests/auth.register.test.js
+++ backend/src/tests/auth.register.test.js
@@ -1,8 +1,13 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
 const app = require("../app");
 
-describe("auth register API", () => {
-  it("should register a new user", async () => {
+describe("Auth Register API", () => {
+  beforeAll(async () => {
+    await mongoose.connection.dropDatabase();
+  });
+
+  it("should register a new user and save to database", async () => {
     const response = await request(app)
       .post("/api/auth/register")
       .send({
@@ -12,5 +17,6 @@ describe("auth register API", () => {
       });
 
     expect(response.statusCode).toBe(201);
+    expect(response.body.email).toBe("sarat@test.com");
   });
 });
